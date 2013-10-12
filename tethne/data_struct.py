5a6d32cf1a5f6f215c0b1e82516357141f35d3cd
rsubra13 <ramkisastra@gmail.com>
2013-10-11 17:45:04 -0700

    Changes in the data_struct docstrings.
    Former-commit-id: eb374d054938f72c18d6148c20e34605f90277f6

--- tethne/data_struct.py
+++ tethne/data_struct.py
@@ -1,6 +1,18 @@
 
 def new_meta_dict():
     """
+    Creates a Meta Dictionary of citation data values. 
+    This function has the most common values from citation data sources like WebOfScience,PubMed etc., 
+    
+    Args:
+        None.
+    
+    Returns:
+        A meta_list dictionary with 'None' as default values.
+    
+    Raises:
+        None.
+        
     aulast  - authors' last name as a list
     auinit  - authors' first initial as a list
     atitle  - article title
@@ -17,6 +29,7 @@ def new_meta_dict():
     doi         - Digital Object Identifier 
     pmid        - PubMed ID
     wosid       - Web of Science UT fieldtag
+    
     """
     meta_dict = {'aulast':None,
                  'auinit':None,
@@ -57,8 +70,20 @@ def new_query_dict():
 def new_wos_dict():
     """
     Defines the set of field tags that will try to be converted into
-    a meta_dict and intializes them to None
-    FIXME: order these a certain way?
+    a meta_dict and intializes them to 'None'.
+   
+    Args:
+        None.
+    
+    Returns:
+        A 'wos_list' dictionary with 'None' as default values.
+    
+    Raises:
+        None.
+    
+    Note:
+        FIXME: order these a certain way?
+   
     """
     wos_dict = {'DI':None,
                 'AU':None,
@@ -76,7 +101,17 @@ def new_wos_dict():
 
 def wos2meta_map():
     """
-    Defines the direct relationships between the wos_dict and the meta_dict
+    Defines the direct relationships between the wos_dict and the meta_dict.
+    
+    Args:
+        None.
+    
+    Returns:
+        A 'translator' dictionary.
+    
+    Raises:
+        None.
+   
     """
     translator = {'DI':'doi',
                   'TI':'atitle',
