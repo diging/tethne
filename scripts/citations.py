f9fdb113459d92da2440dc7139e34e7a6ed9110e
rsubra13 <ramkisastra@gmail.com>
2013-10-15 16:41:31 -0700

    Changes made to test in the Windows environment.
    Former-commit-id: 267eb3743c6be6c4c83b573ce8dcbe07f5b1db74

--- scripts/citations.py
+++ scripts/citations.py
@@ -1,8 +1,9 @@
 #right now this crashes: next step is to make networks not make any attributes
 #with value of None
 import tethne.readers as rd
-filepath = "../docs/savedrecs.txt"
-wos_list = rd.parse_wos(filepath)
+#filepath = "../docs/savedrecs.txt"
+filepath = "C:\Users\Ramki\Downloads\savedrecs.txt"
+wos_list = rd.parse_wos(filepath) 
 meta_list = rd.wos2meta(wos_list)
 
 import tethne.networks as nt
@@ -10,5 +11,5 @@ cites, internal_cites = nt.nx_citations(meta_list, 'ayjid', 'atitle', 'date',
                                         'badkey', 'jtitle') 
 
 import tethne.writers as wr
-wr.to_gexf(cites, "/home/apoh/Repository/output/citations")
-wr.to_gexf(internal_cites, "/home/apoh/Repository/output/internal_citations")
+wr.to_gexf(cites, "C:\Users\Ramki\Downloads\cites_output")
+wr.to_gexf(internal_cites, "C:\Users\Ramki\Downloads\interal_cites_output")
