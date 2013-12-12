fce2a74d42320337a8e792b406eef5cb956ba9cc
Ramki Subramanian <rsubra13@asu.edu>
2013-12-12 15:53:30 -0700

    [#57532340] DAG's advantage for direct_citations
    Former-commit-id: 5e5b7318f100af288f77e42f29ce58ca68bfe99f

--- scripts/citations.py
+++ scripts/citations.py
@@ -1,15 +1,50 @@
 #right now this crashes: next step is to make networks not make any attributes
 #with value of None
+# import tethne.readers as rd
+# #filepath = "../docs/savedrecs.txt"
+# filepath = "../testsuite/testin/cocitations_test_2recs.txt"
+# wos_list = rd.wos.parse_wos(filepath)
+# meta_list = rd.wos.wos2meta(wos_list)
+# 
+# import tethne.networks as nt
+# cites, internal_cites = nt.citations.direct_citation(meta_list, 'ayjid', 'atitle', 'date',
+#                                         'badkey', 'jtitle') 
+# 
+# import tethne.writers as wr
+# wr.graph.to_gexf(cites, "../testsuite/testout/cites" )
+# wr.graph.to_gexf(internal_cites, "../testsuite/testout/internal_cites")
+
+
 import tethne.readers as rd
 #filepath = "../docs/savedrecs.txt"
-filepath = "../testsuite/testin/cocitations_test_2recs.txt"
+filepath = "../testsuite/testin/citations_test.txt"
 wos_list = rd.wos.parse_wos(filepath)
 meta_list = rd.wos.wos2meta(wos_list)
 
 import tethne.networks as nt
-cites, internal_cites = nt.citations.direct_citation(meta_list, 'ayjid', 'atitle', 'date',
-                                        'badkey', 'jtitle') 
+cites, internal_cites = nt.citations.direct_citation(meta_list, 'ayjid', 'atitle', 'date') 
 
-import tethne.writers as wr
-wr.graph.to_gexf(cites, "../testsuite/testout/cites" )
-wr.graph.to_gexf(internal_cites, "../testsuite/testout/internal_cites")
+print "------Cites Nodes----"
+print cites.nodes()
+print "####Cites Edges####"
+print cites.edges()
+print "----Internal Cites--"
+print internal_cites.nodes()
+print internal_cites.edges()
+
+c_des = nt.citations.descendants(cites,'Abdullah S 2012 INFORM SCIENCES')
+i_des = nt.citations.descendants(internal_cites,'ACAMPORA G 2013 INFORMATION SCIENCES')
+
+c_ans = nt.citations.ancestors(cites,'Borda J. 1981 MEMOIRE ELECTIONS SC')
+#i_ans = nt.citations.ancestors(internal_cites,'Borda J. 1981 MEMOIRE ELECTIONS SC')
+
+#print 'cites des:', c_des
+#print 'Internal des:', i_des
+
+print 'cites ans:', c_ans
+#print 'Internal ans:', i_ans
+
+is_directed = cites.is_directed()
+is_dag = nt.citations.is_directed_acyclic_graph(cites)
+print 'Is it directed? :' , is_directed
+print 'Is it dag :' , is_dag
--- tethne/networks/citations.py
+++ tethne/networks/citations.py
@@ -365,3 +365,101 @@ def ancestors(G, source):
         raise nx.NetworkXError("The node %s is not in the graph." % source)
     anc = set(nx.shortest_path_length(G, target=source).keys()) - set([source])
     return anc
+
+
+def is_directed_acyclic_graph(G):
+    """Return True if the graph G is a directed acyclic graph (DAG) or 
+    False if not.
+    
+    Parameters
+    ----------
+    G : NetworkX graph
+      A graph
+
+    Returns
+    -------
+    is_dag : bool
+       True if G is a DAG, false otherwise
+    """
+    try:
+        topological_sort(G)
+        return True
+    except nx.NetworkXUnfeasible:
+        return False
+        
+def topological_sort(G,nbunch=None):
+    """Return a list of nodes in topological sort order.
+
+    A topological sort is a nonunique permutation of the nodes
+    such that an edge from u to v implies that u appears before v in the
+    topological sort order.
+
+    Parameters
+    ----------
+    G : NetworkX digraph
+       A directed graph
+
+    nbunch : container of nodes (optional)
+       Explore graph in specified order given in nbunch
+
+    Raises
+    ------
+    NetworkXError
+       Topological sort is defined for directed graphs only. If the
+       graph G is undirected, a NetworkXError is raised.
+
+    NetworkXUnfeasible
+       If G is not a directed acyclic graph (DAG) no topological sort
+       exists and a NetworkXUnfeasible exception is raised.
+
+    Notes
+    -----
+    This algorithm is based on a description and proof in
+    The Algorithm Design Manual [1]_ .
+
+    See also
+    --------
+    is_directed_acyclic_graph
+
+    References
+    ----------
+    .. [1] Skiena, S. S. The Algorithm Design Manual  (Springer-Verlag, 1998). 
+        http://www.amazon.com/exec/obidos/ASIN/0387948600/ref=ase_thealgorithmrepo/
+    """
+    if not G.is_directed():
+        raise nx.NetworkXError(
+                "Topological sort not defined on undirected graphs.")
+
+    # nonrecursive version
+    seen={}
+    order_explored=[] # provide order and 
+    explored={}       # fast search without more general priorityDictionary
+                     
+    if nbunch is None:
+        nbunch = G.nodes_iter() 
+    for v in nbunch:     # process all vertices in G
+        if v in explored: 
+            continue
+        fringe=[v]   # nodes yet to look at
+        while fringe:
+            w=fringe[-1]  # depth first search
+            if w in explored: # already looked down this branch
+                fringe.pop()
+                continue
+            seen[w]=1     # mark as seen
+            # Check successors for cycles and for new nodes
+            new_nodes=[]
+            for n in G[w]:
+                if n not in explored:
+                    if n in seen: #CYCLE !!
+                        raise nx.NetworkXUnfeasible("Graph contains a cycle.")
+                    new_nodes.append(n)
+            if new_nodes:   # Add new_nodes to fringe
+                fringe.extend(new_nodes)
+            else:           # No new nodes so w is fully explored
+                explored[w]=1
+                order_explored.insert(0,w) # reverse order explored
+                fringe.pop()    # done considering this node
+    return order_explored        
+      
+
