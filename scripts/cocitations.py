b819cac2771f817b3a6332826c69692a4d345054
rsubra13 <rsubra13@asu.edu>
2014-01-01 17:51:48 -0700

    [#63128668] changes in network_test.py and checked with network output. they are correct. ramki@ubuntu:~/git/tethne/testsuite$ python -m unittest -v networks_test test_node_attribs_check (networks_test.TestAuthorCoinstitution) ... ok test_shared_institutions_one (networks_test.TestAuthorCoinstitution) ... ok test_shared_institutions_two (networks_test.TestAuthorCoinstitution) ... ok test_shared_institutions_zero (networks_test.TestAuthorCoinstitution) ... ok
    Former-commit-id: 63f0eea3df56c60b0372fd00e4b5d723e3511998 [formerly f24b8909a317cdee9629fce4c8cc9c3245c8ff5e]
    Former-commit-id: 44827f7da63b6c098559d64f26890e25b69ea986

--- scripts/cocitations.py
+++ scripts/cocitations.py
@@ -1,11 +1,11 @@
 import tethne.readers as rd
-filepath = "../testsuite/testin/cocitations_test.txt"
+filepath = "../testsuite/testin/cocitations_test_2recs.txt"
 wos_list = rd.wos.parse_wos(filepath)
 
 meta_list=rd.wos.wos2meta(wos_list)
 
 import tethne.networks as nt
-coinst = nt.papers.cocitation(meta_list, 1, 2013)
+coinst = nt.papers.cocitation(meta_list, 1)
 
 import tethne.writers as wr
 wr.graph.to_gexf(coinst, "../testsuite/testout/cocitation")
