adf8269c4e5899183a5503cb50810b69ae234cc2
Erick <erick.peirson@asu.edu>
2014-01-02 17:30:54 -0700

    minor
    Former-commit-id: 4656c121853593fedea8a4a6944597fb0e5f8a2d [formerly 3997bde143d7e37a009e42b1a958a90984af6cfe]
    Former-commit-id: b841b15e1c114434b95ca40840f9ff55975b1015

--- tethne/writers/__init__.py
+++ tethne/writers/__init__.py
@@ -3,6 +3,6 @@ Methods for writing datasets to commonly-used data formats, for visualization
 and analysis in external applications, and for storage.
 """
 
-#import tethne.writers.graph as graph
-#import tethne.writers.matrix as matrix
-#import tethne.writers.collection as collection
+import graph
+import matrix
+import collection
