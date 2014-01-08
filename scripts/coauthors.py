45e3d63ec1fa90c8f87aa878dfa0311b48058843
Ramki Subramanian <rsubra13@asu.edu>
2014-01-07 17:38:32 -0700

    [#62808308] changes done in coauthors.
    The Edge attributes list of dictionaries is not getting populated
    properly. Checking on that issue.
    
    
    Former-commit-id: 87e73b7be5f979ced3a27b0f2b1deff2065dc7a4 [formerly e4d8a4257f3f313443d81b905ca858366f79c494]
    Former-commit-id: 990d8bf74520b2725eee22ba4c4290e8421471d6

--- scripts/coauthors.py
+++ scripts/coauthors.py
@@ -1,11 +1,15 @@
 import tethne.readers as rd
-filepath = "../testsuite/testin/instituitions_2_types_input.txt"
+filepath = "../testsuite/testin/coauthors_2_recs.txt"
 wos_list = rd.wos.parse_wos(filepath)
 meta_list = rd.wos.wos2meta(wos_list)
 
 import tethne.networks as nt
-coauthors = nt.authors.coauthors(meta_list, 'citations', 'date', 'badkey', 'jtitle')
+coauthors = nt.authors.coauthors(meta_list, 'date','jtitle','ayjid')
 
-print "comes here"
+print "comes here################"
 import tethne.writers as wr
-wr.graph.to_sif(coauthors, "../testsuite/testout/coauthors")
+wr.graph.to_gexf(coauthors, "../testsuite/testout/coauthors_code_jan07")
+
+print coauthors.nodes()
+print '@@@@@@@@edges@@@@@'
+print coauthors.edges()
