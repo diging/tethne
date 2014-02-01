64bd2b8fe0945ee4d4cbccebe463f448cdcf72ae
Erick <erick.peirson@asu.edu>
2014-02-01 10:28:01 -0700

    removed refs to missing sub packages
    Former-commit-id: 7d02e1125b8edad5658c76eea592279f036f4a47 [formerly 41f914a288eadb6e817c4eff457f0f13d06742d6]
    Former-commit-id: ad34b7f4a7324dfe4c9181cfc2452f26d271a54d

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from distutils.core import setup
 DISTNAME = 'tethne'
 MAINTAINER = 'Erick Peirson'
 MAINTAINER_EMAIL = 'erick [dot] peirson [at] asu [dot] edu'
-DESCRIPTION = ('A set of Python modules for constructing graphs from the Web ' +
-                'of Science')
-LICENSE = 'new BSD' 
+DESCRIPTION = ('A set of Python modules for constructing graphs from the Web ' \
+                + 'of Science')
+LICENSE = 'GNU GPL 3' 
 URL = 'https://github.com/erickpeirson/tethne' 
-VERSION = '0.2' 
+VERSION = '0.2'
 LONG_DESCRIPTION = open('README.md').read()
-PACKAGES = ['tethne','tethne.analyze','tethne.networks','tethne.readers','tethne.resources','tethne.services','tethne.utilities', 'tethne.writers','testsuite']
+PACKAGES = [ 'tethne','tethne.analyze','tethne.networks','tethne.readers', \
+             'tethne.utilities', 'tethne.writers','testsuite' ]
 
 setup(name=DISTNAME,
       maintainer=MAINTAINER,
