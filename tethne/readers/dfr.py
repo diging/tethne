83664ffa2c79e1b3f4853354383f220d77cfd99b
Erick <erick.peirson@asu.edu>
2014-02-03 14:38:51 -0700

    more tests
    Former-commit-id: 78de71dae8809961f47c4e1ecb28789d56b5fa0d

--- tethne/readers/dfr.py
+++ tethne/readers/dfr.py
@@ -7,38 +7,38 @@ from tethne.utilities import dict_from_node, strip_non_ascii
 def read(datapath):
     """
     Yields :class:`.Paper`s from JSTOR DfR package.
-    
+
     Parameters
     ----------
     filepath : string
         Filepath to unzipped JSTOR DfR folder containing a citations.XML file.
-    
+
     Returns
     -------
     papers : list
-        A list of :class:`.Paper` objects.    
+        A list of :class:`.Paper` objects.
     """
-    
+
     try:
         root = ET.parse(datapath + "/citations.XML").getroot()
     except IOError:
         raise IOError(datapath+"citations.XML not found.")
-        
+
     papers = []
     for article in root:
         papers.append(_handle_paper(article))
-    
+
     return papers
-        
+
 def _handle_paper(article):
     """
     Yields a :class:`.Paper` from an article ET node.
-    
+
     Parameters
     ----------
     article : Element
         ElementTree Element 'article'.
-        
+
     Returns
     -------
     paper : :class:`.Paper`
@@ -56,31 +56,31 @@ def _handle_paper(article):
 
     # Handle author names.
     paper['aulast'], paper['auinit'] = _handle_authors(pdata['author'])
-    
+
     # Handle pubdate.
     paper['date'] = _handle_pubdate(pdata['pubdate'])
-    
+
     # Handle pagerange.
     paper['spage'], paper['epage'] = _handle_pagerange(pdata['pagerange'])
-    
+
     # Generate ayjid.
     try:
         paper['ayjid'] = _create_ayjid(paper['aulast'][0], paper['auinit'][0], \
                                        paper['date'], paper['jtitle'])
     except IndexError:  # Article may not have authors.
         pass
-    
+
     return paper
 
 def _handle_pagerange(pagerange):
     """
     Yields start and end pages from DfR pagerange field.
-    
+
     Parameters
     ----------
     pagerange : str or unicode
         DfR-style pagerange, e.g. "pp. 435-444".
-        
+
     Returns
     -------
     start : str
@@ -88,31 +88,31 @@ def _handle_pagerange(pagerange):
     end : str
         End page.
     """
-    
+
     try:
         pr = re.compile("pp\.\s([0-9]+)\-([0-9]+)")
         start, end = re.findall(pr, pagerange)[0]
     except IndexError:
         start = end = 0
-    
+
     return str(start), str(end)
 
 def _handle_pubdate(pubdate):
     """
     Yields a date integer from DfR pubdate field.
     """
-    
+
     return int(pubdate[0:4])
-    
+
 def _handle_authors(authors):
     """
     Yields aulast and auinit lists from value of authors node.
-    
+
     Parameters
     ----------
     authors : list, str, or unicode
         Value or values of 'author' element in DfR XML.
-        
+
     Returns
     -------
     aulast : list
@@ -120,7 +120,7 @@ def _handle_authors(authors):
     auinit : list
         A list of author first-initials (string).
     """
-    
+
     aulast = []
     auinit = []
     if type(authors) is list:
@@ -137,7 +137,7 @@ def _handle_authors(authors):
         try:
             l,i = _handle_author(author)
             aulast.append(l)
-            auinit.append(i)        
+            auinit.append(i)
         except ValueError:
             pass
     else:
@@ -148,12 +148,12 @@ def _handle_authors(authors):
 def _handle_author(author):
     """
     Yields aulast and auinit from an author's full name.
-    
+
     Parameters
     ----------
     author : str or unicode
         Author fullname, e.g. "Richard L. Nixon".
-    
+
     Returns
     -------
     aulast : str
@@ -161,7 +161,7 @@ def _handle_author(author):
     auinit : str
         Author first-initial.
     """
-    
+
     lname = author.split(' ')
 
     try:
@@ -173,12 +173,12 @@ def _handle_author(author):
             aulast = final
     except IndexError:
         raise ValueError("malformed author name")
-        
+
     return str(aulast), str(auinit)
-    
+
 def _dfr2paper_map():
     """
-    Defines the direct relationships between DfR article elements and 
+    Defines the direct relationships between DfR article elements and
     :class:`.Paper` fields.
 
     Returns
@@ -186,15 +186,15 @@ def _dfr2paper_map():
     translator : dict
         A 'translator' dictionary.
     """
-    
+
     translator = {  'doi': 'doi',
                     'title': 'atitle',
                     'journaltitle': 'jtitle',
                     'volume': 'volume',
                     'issue': 'issue'    }
-                    
+
     return translator
-    
+
 def _create_ayjid(aulast=None, auinit=None, date=None, jtitle=None, **kwargs):
     """
     Convert aulast, auinit, and jtitle into the fuzzy identifier ayjid
@@ -241,14 +241,13 @@ def _create_ayjid(aulast=None, auinit=None, date=None, jtitle=None, **kwargs):
     if ayj == '   ':
         ayj = 'Unknown paper'
 
-    return ayj.upper()    
+    return ayj.upper()
 
-    
+
 if __name__ == "__main__":
     from pprint import pprint
-    
+
     path = "/Users/erickpeirson/Downloads/DfR/ecology_1960-64"
     papers = read(path)
-    
+
     pprint(papers[0].__dict__)
-    
