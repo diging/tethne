6a3df13a779d9cbfff49501b4b7169d2ff1951e9
Erick <erick.peirson@asu.edu>
2014-02-03 11:17:56 -0700

    documentation
    Former-commit-id: 9cae36fa42b165720e5feeb3638e73c5772d7145 [formerly 4225882173436da3239875d97e8a4715da614ca2]
    Former-commit-id: 145e98d3d9900082df8b9b02b9b6fc7226175882

--- tethne/readers/__init__.py
+++ tethne/readers/__init__.py
@@ -1,5 +1,36 @@
 """
-Contains methods for parsing bibliographic datasets.
+Each file reader provides methods to parse bibliographic data from a
+scholarly database (e.g. Web of Science or PubMed), resulting in a
+list of :class:`.Paper` instances containing as many as possible of
+the following keys (missing values are set to None):
+
+    * aulast (list) -- Authors' surnames, as a list.
+    * auinit (list) -- Authors' initials, as a list.
+    * institution (dict) -- Institutions with which the authors are affiliated.
+    * atitle (str) -- Article title.
+    * jtitle (str) -- Journal title or abbreviated title.
+    * volume (str) -- Journal volume number.
+    * issue (str) -- journal issue number.
+    * spage (str) -- Etarting page of article in journal.
+    * epage (str) -- Ending page of article in journal.
+    * date (int) -- Date of publication.
+    * abstract (str)
+
+These keys are associated with the meta data entries in the databases of
+organizations such as the International DOI Foundation and its Registration
+Agencies such as CrossRef and DataCite.
+
+In addition, :class:`.Paper` instances will contain keys with information
+relevant to the networks of interest for Tethne including:
+
+    * citations -- list of minimum :class:`.Paper` instances for cited
+        references.
+    * ayjid -- First author's name (last, fi), publication year, and journal.
+    * doi -- Digital Object Identifier
+    * pmid -- PubMed ID
+    * wosid -- Web of Science UT fieldtag
+
+Missing data here also results in the above keys being set to None.
 """
 
 import wos
