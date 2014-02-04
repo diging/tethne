ef0ae3aa2fdcdb011741dd65d198e967ee2d7a7a
Erick <erick.peirson@asu.edu>
2014-02-04 07:53:23 -0700

    finished array and matrix methods for dfr
    Former-commit-id: b732c3b920549b5800d486894055d0ee90e646d6 [formerly fb0333a3cc750abf961f92407d39693ae877d1dc]
    Former-commit-id: 9aaf755afb269833aafadd7a2d2dac97b3abb862

--- tethne/matrices/dfr.py
+++ tethne/matrices/dfr.py
@@ -5,6 +5,7 @@ Methods for generating Numpy data objects from JSTOR Data-for-Research datasets.
 
    array
    matrix
+
 """
 
 import numpy as np
@@ -13,11 +14,11 @@ class Map(object):
     """
     Maps integer indices to string values.
     """
-    
+
     def __init__(self):
         self.by_str = {}
         self.by_int = {}
-        
+
     def __setitem__(self, key, value):
         if type(key) == str:
             self.by_str[key] = value
@@ -25,20 +26,20 @@ class Map(object):
         if type(key) == int:
             self.by_int[key] = value
             self.by_str[value] = key
-    
+
     def __getitem__(self, key):
         if type(key) == str:
             return self.by_str[key]
         if type(key) == int:
             return self.by_int[key]
-            
+
     def __len__(self):
         return len(self.by_str)
 
 def array(data, normalize=False, verbose=False):
     """
     Yields a Numpy array, along with feature-index and document-index mappings.
-    
+
     Parameters
     ----------
     data : dict
@@ -46,7 +47,7 @@ def array(data, normalize=False, verbose=False):
         frequency tuples.
     normalize : bool
         If True, matrix values are relative to the maximum value in the matrix.
-    
+
     Returns
     -------
     A : Numpy array
@@ -60,38 +61,42 @@ def array(data, normalize=False, verbose=False):
     document_index, feature_index = _index_data(data, verbose)
     N_docs = len(document_index)
     N_feat = len(feature_index)
-    
-    if verbose: 
-        print "array(): %c documents, %c features".format(N_docs, N_feat)
-                        
-    A = np.zeros((N_docs, N_feat))
+    max_v = np.max([ np.float(v[1]) for doc in data.keys() for v in data[doc] ])
+
+    if verbose:
+        print "array(): {0} documents, {1} features".format(N_docs, N_feat)
+
+    if normalize:
+        dtype = np.float
+    else:
+        dtype = np.int
+
+    A = np.zeros((N_docs, N_feat), dtype=dtype)
     d = 0
-    
+
     for doc, features in data.iteritems():
         if verbose:
             d += 1
-            if d%100 == 0:
-                print "array(): Processed %c of %c documents".format(d, N_docs)
-                
+            if d%200 == 0:
+                print "array(): Processed {0} of {1} documents"\
+                                                              .format(d, N_docs)
+
         i = document_index[doc]
         for f, v in features:
             j = feature_index[f]
+            if normalize:
+                v = v/max_v
             A[i, j] = v
-    
+
     if verbose:
         print "array(): Processed all documents."
-    
-    if normalize:
-        if verbose:
-            print "array(): Normalizing."
-        A = A / np.max(A)
 
     return A, document_index, feature_index
 
 def matrix(data, normalize=False, verbose=False):
     """
     Yields a Numpy matrix, along with feature-index and document-index mappings.
-    
+
     Parameters
     ----------
     data : dict
@@ -99,7 +104,7 @@ def matrix(data, normalize=False, verbose=False):
         frequency tuples.
     normalize : bool
         If True, matrix values are relative to the maximum value in the matrix.
-    
+
     Returns
     -------
     M : Numpy matrix
@@ -111,21 +116,27 @@ def matrix(data, normalize=False, verbose=False):
 
     """
 
+    if verbose:
+        print "matrix(): converting array to matrix."
+
     A, document_index, feature_index = array(data, normalize, verbose)
-    M = np.matrix(A)
-    
+    M = np.asmatrix(A)
+
+    if verbose:
+        print "matrix(): done."
+
     return M, document_index, feature_index
 
 def _index_data(data, verbose=False):
     """
     Yields document and feature indices from a data dict.
-    
+
     Parameters
     ----------
     data : dict
         Keys are document identifiers (e.g. DOIs), values are lists of feature-
-        frequency tuples.    
-        
+        frequency tuples.
+
     Returns
     -------
     document_index : class:`.Map`
@@ -133,19 +144,20 @@ def _index_data(data, verbose=False):
     feature_index : :class:`.Map`
         Maps integer row indices to features.
     """
-            
+
     document_index = Map()
     feature_index = Map()
     N_docs = len(data)
-    
+
     if verbose:
-        print "_index_data(): Indexing %c documents".format(N_docs)
-    
+        print "_index_data(): Indexing {0} documents".format(N_docs)
+
     features = set()
     for i in xrange(N_docs): # Index documents.
-        if verbose and i%100 == 0:
-            print "_index_data(): Indexed %c of %c documents".format(i, N_docs)
-            
+        if verbose and i%200 == 0:
+            print "_index_data(): Indexed {0} of {1} documents"\
+                                                              .format(i, N_docs)
+
         document_index[i] = data.keys()[i]
         for f,c in data.values()[i]:
             features.add(f) # Build a set of features.
@@ -153,28 +165,17 @@ def _index_data(data, verbose=False):
     N_feat = len(features)
     if verbose:
         print "_index_data(): Done indexing documents."
-        print "_index_data(): Indexing %c features.".format(N_feat)
-    
+        print "_index_data(): Indexing {0} features.".format(N_feat)
+
     features = list(features)
     for i in xrange(N_feat):  # Index features.
-        if verbose and i %100 == 0:
-            print "_index_data(): Indexed %c of %c features.".format(i, N_feat)
-            
+        if verbose and i %10000 == 0:
+            print "_index_data(): Indexed {0} of {1} features."\
+                                                              .format(i, N_feat)
+
         feature_index[i] = features[i]
-    
+
     if verbose:
         print "_index_data(): Done indexing features."
-    
-    return document_index, feature_index
-    
-if __name__ == '__main__':
-    import tethne.readers as rd
-    
-    path = "/Users/erickpeirson/Downloads/DfR/ecology_1960-64"
-#    papers = read(path)
-    bigrams = rd.dfr.ngrams(path)
 
-    print len(bigrams)
-    
-    M = matrix(bigrams, verbose=True)
-    print M
+    return document_index, feature_index
