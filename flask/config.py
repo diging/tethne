e5c8a8d238ab1f8c23f8c1a0582a4a473163102a
Ramki Subramanian <rsubra13@asu.edu>
2014-02-11 17:17:43 -0700

    folder
    Former-commit-id: eff2d6c81bee74fb24d1e0bb19a3a2599d997b37

---
+++ flask/config.py
@@ -0,0 +1,13 @@
+import os
+
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+
+# ZODB TO BE ADDED HERE
+
+# Connect to the database
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
