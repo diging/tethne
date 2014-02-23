6a1f587012186da3bcba6440b61619a39ce16a9f
Erick <erick.peirson@asu.edu>
2014-02-23 12:52:03 -0700

    minor, related to PyPI submission
    Former-commit-id: 11019a5f811390bbafc4b30514ddb7504cca56f3

--- setup.py
+++ setup.py
@@ -3,12 +3,10 @@ from distutils.core import setup
 DISTNAME = 'tethne'
 MAINTAINER = 'Erick Peirson'
 MAINTAINER_EMAIL = 'erick [dot] peirson [at] asu [dot] edu'
-DESCRIPTION = ('A set of Python modules for constructing graphs from the Web ' \
-                + 'of Science')
+DESCRIPTION = ('Python library for analyzing bibliographic data')
 LICENSE = 'GNU GPL 3' 
 URL = 'https://github.com/erickpeirson/tethne' 
-VERSION = '0.3'
-LONG_DESCRIPTION = open('README.md').read()
+VERSION = '0.3.0-alpha'
 PACKAGES = [ 'tethne','tethne.analyze','tethne.networks','tethne.readers', \
              'tethne.utilities', 'tethne.writers', 'tethne.matrices',
              'testsuite' ]
@@ -20,7 +18,5 @@ setup(name=DISTNAME,
       license=LICENSE,
       url=URL,
       version=VERSION,
-      long_description=LONG_DESCRIPTION,
       packages = PACKAGES,
-      install_requires = ['networkx >= 1.8']
       )
