b5e4112d5b75671ab2ba3a292efa2c0e861dd844
Erick <erick.peirson@asu.edu>
2014-02-25 10:15:24 -0700

    added --local flag
    Former-commit-id: c85b86989f2cb4b9d3520fdf7779ace9d409f5fa

--- tethne/__main__.py
+++ tethne/__main__.py
@@ -84,6 +84,9 @@ if __name__ == "__main__":
                       help="Path to save workflow output. Some workflow steps"+\
                            " will generate summary statistics or other output.")
 
+    parser.add_option("-L", "--local",
+                      action="store_true", dest="local", default=False)
+
     # Options for read workflow step.
     readGroup = OptionGroup(parser, "Options for read workflow step")
     
@@ -203,6 +206,10 @@ if __name__ == "__main__":
     
     (options, args) = parser.parse_args()
     
+    if options.local:
+        lpath = os.path.dirname(os.path.abspath(__file__))
+        sys.path.append(lpath + "/..")
+        print sys.path
     
     if options.dataset_id is None:
         sys.exit('Must specify --dataset-id')
