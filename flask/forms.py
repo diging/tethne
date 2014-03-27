46daaa3fc9c984a8e2d6301dde726f0ee43dbfd1
Ramki Subramanian <rsubra13@asu.edu>
2014-03-26 18:14:12 -0700

    [#65600668] [#68321330] [#66875220]
    [#68321330] - Crete Datasets :  get the details from the user.
    
    [#66875220] restricting user views.
    
    
    Former-commit-id: 300040f43f5ae9c81f4c83902eee8c58bb0193b6

--- flask/forms.py
+++ flask/forms.py
@@ -9,18 +9,11 @@ from ZODB import FileStorage, DB
 import models as mod
 # Set your classes here.
 
-# storage = FileStorage('users.fs')
-# db = DB(storage)
-# connection = db.open()
-# # dbroot is a dict like structure.
-# dbroot = connection.root()  # retrieving the root of the tree
-# #  
-# if not dbroot.has_key('userdb'):
-#     from BTrees.OOBTree import OOBTree
-#     dbroot['userdb'] = OOBTree()
 
 class RegisterForm(Form):
+    """
     
+    """
     #Need to change the email using Email validator.
     
     name             = TextField('Username', [validators.Required(),validators.Length(min=4, max=35)])
@@ -32,16 +25,29 @@ class RegisterForm(Form):
     security_answer  = TextField('Security Answer', [validators.Required(), validators.Length(min=4, max=40)])
     
     
-    
-#     def __repr__(self):
-#         print "comes here:"
-#         return '<User %r>' % (userobj.name)
-
 class LoginForm(Form):
+    """
+    
+    """
     name        = TextField('Username', [validators.Required()])
     password    = PasswordField('Password', [validators.Required()])
 
 class ForgotForm(Form):
+    """
+    
+    """
     email       		= TextField('Email', [validators.Required(), validators.Length(min=6, max=40)] )
     security_question	= TextField('Security Question', [validators.Required(), validators.Length(min=10, max=40)])
     security_answer  	= TextField('Security Answer', [validators.Required(), validators.Length(min=6, max=40)])
+    
+
+class UserDetailsForm(Form):
+    """
+    
+    """
+    #Need to change the email using Email validator.
+    
+    name             = TextField('Username', [validators.Required(),validators.Length(min=4, max=35)])
+    email            = TextField('Email', [validators.Required(),validators.Length(min=4, max=35)])
+    institution      = TextField('Institution', [validators.Required(), validators.Length(min=4, max=40)])
+   
