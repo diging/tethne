6c3a128fb03a460a37d26edf0b5021b8df21d3b4
Erick <erick.peirson@asu.edu>
2014-04-11 08:57:02 -0700

    corpus prep for DTM
    Former-commit-id: e21b69c129cae345840df27fc9074a7431bd15b9 [formerly 2b8d9953e65ccfa12db9a2669b66f68bc71f36b3]
    Former-commit-id: 5ca5fafcbe1bfe8739dbcced721db7c6c8561539

--- tethne/writers/corpora.py
+++ tethne/writers/corpora.py
@@ -1,6 +1,8 @@
 """
 """
 
+from collections import Counter
+
 def to_documents(target, ngrams):
     """
     
@@ -18,14 +20,14 @@ def to_documents(target, ngrams):
     
     Raises
     ------
-    ValueError
+    IOError
     """
     
     try:
         docFile = open(target + '_docs.txt', 'wb')
         metaFile = open(target + '_meta.csv', 'wb')
     except IOError:
-        raise ValueError('Invalid target. Could not open files for writing.')
+        raise IOError('Invalid target. Could not open files for writing.')
     
     metaFile.write('# doc\tdoi\n')
     
@@ -44,3 +46,88 @@ def to_documents(target, ngrams):
     metaFile.close()
     
     return
+
+def to_dtm_input(target, D, t_ngrams, vocab):
+    """
+    
+    Parameters
+    ----------
+    D : :class:`.DataCollection`
+        Contains :class:`.Paper` objects generated from the same DfR dataset
+        as t_ngrams, indexed by doi and sliced by date.
+    target : str
+        Target path for documents; e.g. './mycorpus' will result in 
+        './mycorpus-mult.dat', './mycorpus-seq.dat', 'mycorpus-vocab.dat', and
+        './mycorpus-meta.dat'.
+    t_ngrams : dict
+        Keys are paper DOIs, values are lists of (index, frequency) tuples.
+        
+    Returns
+    -------
+    None : If all goes well.
+    
+    Raises
+    ------
+    IOError
+    """
+
+    try:
+        metaFile = open(target + '-meta.dat', 'wb')
+    except IOError:
+        raise IOError('Invalid target. Could not open files for writing.')
+
+    # Generate -mult.dat file (wordcounts for each document).
+    #   From the DTM example:
+    #
+    #     one-doc-per-line, each line of the form
+    #         unique_word_count index1:count1 index2:count2 ... indexn:counnt
+    #     The docs in foo-mult.dat should be ordered by date, with the first
+    #     docs from time1, the next from time2, ..., and the last docs from
+    #     timen.
+    #
+    # And -meta.dat file (with DOIs).
+    #
+    #       a file with information on each of the documents, arranged in
+    #           the same order as the docs in the mult file.
+    #
+    with open(target + '-meta.dat', 'wb') as metaFile:
+        with open(target + '-mult.dat', 'wb') as multFile:
+            for year, papers in sorted(D['date'].items()):
+                for doi in papers:  # D must be indexed by doi.
+                    grams = t_ngrams[doi]
+                    wordcount = len(grams)  # Number of unique words.
+                    multFile.write(' '.join([ str(wordcount) ] + \
+                                            [ '{0}:{1}'.format(g,c)
+                                                for g,c in grams ] + \
+                                            ['\n']))
+                    metaFile.write('{0}\n'.format(doi))
+
+    # Generate -seq.dat file (number of papers per year).
+    #   From the DTM example:
+    #
+    #       Number_Timestamps
+    #       number_docs_time_1
+    #       ...
+    #       number_docs_time_i
+    #       ...
+    #       number_docs_time_NumberTimestamps
+    #
+    with open(target + '-seq.dat', 'wb') as seqFile:
+        seqFile.write(str(len(D['date'])) + '\n')
+        for year, papers in sorted(D['date'].items()):
+            seqFile.write('{0}\n'.format(len(papers)))
+
+    #       a file with all of the words in the vocabulary, arranged in
+    #       the same order as the word indices
+    with open(target + '-vocab.dat', 'wb') as vocabFile:
+        for index,word in sorted(vocab.items()):
+            vocabFile.write('{0}\n'.format(word))
+
+    return None
+
+if __name__ == '__main__':
+    import sys
+    sys.path.append("/Users/erickpeirson/Dropbox/DigitalHPS/Scripts/tethne")
+
+    import tethne.readers as rd
+    papers = rd.dfr.read("")
