94bca637c38728ab64984ced918c166756fd2b6f
Erick <erick.peirson@asu.edu>
2014-05-27 20:18:30 -0700

    mallet lda
    Former-commit-id: 970077861094460cf2ae81186e62d56b376fbeff [formerly 043c917e056b674a1f66ca1219c50e7ef6d07c33]
    Former-commit-id: cbbfc9efb08f8d0f69a37c1339034f02c7d0b5fc

--- tethne/model/corpus/ldamodel.py
+++ tethne/model/corpus/ldamodel.py
@@ -3,33 +3,33 @@ import numpy as np
 
 class LDAModel(BaseModel):
     """
-    Organizes parsed output from MALLET's LDA modeling algorithm.
+    Latent Dirichlet Allocation topic model.
     
-    Used by :mod:`.readers.mallet`\.
+    Items are documents, dimensions are topics.
+    
+    See: http://jmlr.org/papers/v3/blei03a.html
     """
 
-    def __init__(self, doc_topic, top_word, top_keys, metadata, vocabulary):
+    def __init__(self, theta, phi, metadata, vocabulary=None):
         """
         Initialize the :class:`.LDAModel`\.
         
         Parameters
         ----------
-        doc_top : Numpy matrix
-            Rows are documents, columns are topics. Values indicate the 
-            contribution of a topic to a document, such that all rows sum to 
-            1.0.
-        top_word : Numpy matrix
-            Rows are topics, columns are words. Values indicate the normalized
-            contribution of each word to a topic, such that rows sum to 1.0.
-        top_keys : dict
-            Maps matrix indices for topics onto the top words in that topic.
+        theta : matrix-like
+            Distribution of topics (cols) in documents (rows). Rows sum to 1.
+        phi : matrix-like
+            Distribution over words (cols) for topics (rows). Rows sum to 1.
         metadata : dict
             Maps matrix indices for documents onto a :class:`.Paper` key.
         """
         
-        self.doc_topic = doc_topic
-        self.top_word = top_word
-        self.top_keys = top_keys
+        self.theta = theta
+        self.M = theta.shape[0] # Number of documents.
+
+        self.phi = phi
+        self.Z = phi.shape[0]   # Number of topics.
+
         self.metadata = metadata
         self.vocabulary = vocabulary
         
@@ -40,8 +40,8 @@ class LDAModel(BaseModel):
         """
         Yields proportion of each topic in document.
         """
-        this_prop = self.doc_topic[i, :]
-        return [ (t, this_prop[t]) for t in xrange(this_prop.size) ]
+        theta_i = self.theta[i, :]
+        return [ (t, theta_i[t]) for t in xrange(theta_i.size) ]
     
     def _item_relationship(self, i, j, **kwargs):
         """
@@ -51,16 +51,16 @@ class LDAModel(BaseModel):
 
         return None
     
-    def _dimension_description(self, d, **kwargs):
+    def _dimension_description(self, k, **kwargs):
         """
         Yields probability distribution over terms.
         """
-        this_theta = self.top_word[z, :]
-        return [ (w, this_theta[w]) for w in xrange(this_theta.size) ]
+        phi_k = self.phi[k, :]
+        return [ (w, phi_k[w]) for w in xrange(phi_k.size) ]
     
-    def _dimension_relationship(self, d, e, **kwargs):
+    def _dimension_relationship(self, k, e, **kwargs):
         """
-        Simply returns (d,e); there is no additional information about 
+        Simply returns (k,e); there is no additional information about
         dimensions.
         """
         # TODO: imlement a similarity metric for topics.
@@ -85,7 +85,7 @@ class LDAModel(BaseModel):
 #        """
 #        
 #        index = self.lookup[d]
-#        td = self.doc_topic[index, :]
+#        td = self.theta[index, :]
 #        
 #        if topZ is None:
 #            topZ = td.shape[0]
@@ -106,9 +106,9 @@ class LDAModel(BaseModel):
 #            topW = 5
 #
 #        if type(topW) is int:
-#            words = self.top_word[z,:].argsort()[-topW:][::-1]
+#            words = self.phi[z,:].argsort()[-topW:][::-1]
 #
-#        return [ ( w, self.top_word[z,w]) for w in words ]
+#        return [ ( w, self.phi[z,w]) for w in words ]
 
     def print_topic(self, z):
         words = [ self.vocabulary.by_int[w] for w,p
@@ -120,7 +120,7 @@ class LDAModel(BaseModel):
         """
         Prints a list of topics.
         """
-        Z = self.top_word.shape[0]
+        Z = self.phi.shape[0]
         
         for z in xrange(Z):
             print z, ', '.join(self.words_in_topic(z))
@@ -142,7 +142,7 @@ class LDAModel(BaseModel):
         documents : list
             List of (document, proportion) tuples.
         """    
-        td = self.doc_topic[:, z]
+        td = self.theta[:, z]
         
         if topD is None:
             topD = td.shape[0]
@@ -158,3 +158,134 @@ class LDAModel(BaseModel):
         documents = zip(top_idents, top_values)
         
         return documents
+
+
+
+def from_mallet(top_doc, word_top, metadata=None, metadata_key='doi'):
+    """
+    Parse results from LDA modeling with MALLET.
+
+    MALLET's LDA topic modeling algorithm produces a collection of output files.
+    :func:`.read` takes the topic-document and (sparse) word-topic matrices, as
+    tab-separated value files, along with a metadata file that maps
+    each MALLET document id to a :class:`.Paper`\, using the `metadata_key`.
+
+    Parameters
+    ----------
+    top_doc : string
+        Path to topic-document datafile generated with --output-doc-topics.
+    word_top : string
+        Path to word-topic datafile generated with --word-topic-counts-file.
+    metadata : string (optional)
+        Path to tab-separated metadata file with IDs and :class:`.Paper` keys.
+
+    Returns
+    -------
+    ldamodel : :class:`.LDAModel`
+
+    """
+
+    theta = _handle_top_doc(top_doc)
+    phi = _handle_word_top(word_top)
+
+    if metadata is not None:
+        md = _handle_metadata(metadata)
+    else:
+        md = None
+
+    return LDAModel(theta, phi, md)
+
+def _handle_top_doc(path):
+    """
+    
+    
+    Returns
+    -------
+    td : Numpy array
+        Rows are documents, columns are topics. Rows sum to ~1.
+    """
+    from scipy.sparse import coo_matrix
+
+    D = []
+    T = []
+    P = []
+
+    with open(path, "rb") as f:
+        i = -1
+        reader = csv.reader(f, delimiter='\t')
+        for line in reader:
+            i += 1
+            if i == 0: continue     # Avoid header row.
+            
+            d = int(line[0])
+            t = line[2:]
+            tops = []
+            for i in xrange(0,len(t)-1,2):
+                tops.append( (int(t[i]), float(t[i+1])) )
+        
+            for k,p in tops:
+                D.append(d)     # Document indices.
+                T.append(k)     # Topic indices.
+                P.append(p)     # Proportions.
+
+        M = len(set(D)) # Number of documents.
+        K = len(set(T)) # Number of topics.
+
+    return coo_matrix((P, (D,T)), shape=(M,K)).todense()
+    
+def _handle_word_top(path):
+    """
+    Returns
+    -------
+    wt : Numpy array
+        Rows are topics, columns are words. Rows sum to ~1.
+    vocabulary : :class:`.Dictionary`
+        Maps words to word-indices in wt.
+    """
+    from scipy.sparse import coo_matrix
+    
+    words = {}
+    topics = set()
+
+    W = []
+    T = []
+    C = []
+    
+    with open(path, "rb") as f:
+        reader = csv.reader(f, delimiter=' ')
+        for line in reader: # TODO: figure out why this is so complicated.
+            w = int(line[0])
+            for l in line[2:]:
+                k,c = l.split(':')
+                W.append(w)
+                T.append(int(k))
+                C.append(float(c))
+
+    K = len(set(T))
+    V = len(set(W))
+                
+    phi = coo_matrix((C, (T,W)), shape=(K,V)).todense()
+
+    # Normalize
+    for k in xrange(K):
+        phi[k,:] /= np.sum(phi[k,:])    
+    
+    return phi
+
+def _handle_metadata(path):
+    """
+    Returns
+    -------
+    md : dict
+        Keys are document indices, values are identifiers from a :class:`.Paper`
+        property.
+    """
+    md = {}
+
+    with open(path, "rU") as f:
+        reader = csv.reader(f, delimiter='\t')
+        lines = [ l for l in reader ][1:]
+        for l in lines:
+            md[int(l[0])] = l[1]
+
+    return md
