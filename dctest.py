79a17e856d813618a9ef6ce1679f7bd552466874
Erick <erick.peirson@asu.edu>
2014-05-29 10:08:18 -0700

    cleanup
    Former-commit-id: b0b7813af05e3f8a548e18be8893d6c81806e60e [formerly 7eb82f4e96b9679717ed14703b288dfa8be4a930]
    Former-commit-id: fdd683c9bb98a283b5e74841511916dcfa92afab

--- dctest.py
+++
@@ -1,7 +0,0 @@
-from tethne.managers import SampleDFRManager, GenericGraphCollectionManager
-datapath = '/Users/erickpeirson/tethne/testsuite/refactor/data/dfr'
-MN = SampleDFRManager(datapath)
-MN.run()
-
-GM = GenericGraphCollectionManager(MN.D, 'date', 'authors', 'coauthors')
-GM.run()
