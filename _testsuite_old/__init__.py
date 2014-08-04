5df8bfaa2e1d9262c00eef3be26ce2fa5eddaa4e
Erick <erick.peirson@asu.edu>
2014-08-03 17:53:44 -0700

    cleanup
    Former-commit-id: f2cc787d259d645113f1c4fd21510f58b89339ab

--- _testsuite_old/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Tethne test suite
-"""
--- flask_backups/flask_old/old_flask_folder/admin.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask import * 
-from flask.ext.admin import Admin
-
-app = Flask(__name__)
-
-admin = Admin(app)
-app.run()
