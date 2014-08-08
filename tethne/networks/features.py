f976831d61cb13b1f2fbabf5482f7a05915e5e93
Erick <erick.peirson@asu.edu>
2014-08-07 19:22:49 -0700

    added networks.topics.distance + tests and docs
    Former-commit-id: e9adebd258dbf8cf89c40ac7c43487049939ab94 [formerly 4d188b7fbb8bd7f902969281305aa60f8c7dd860]
    Former-commit-id: 497c7adb472fe9b4d75a31ca56e70fac83f668bb

--- tethne/networks/features.py
+++ tethne/networks/features.py
@@ -15,7 +15,7 @@ includes keywords, abstract terms, etc.
 import logging
 logging.basicConfig(filename=None, format='%(asctime)-6s: %(name)s - %(levelname)s - %(module)s - %(funcName)s - %(lineno)d - %(message)s')
 logger = logging.getLogger(__name__)
-logger.setLevel('DEBUG')
+logger.setLevel('INFO')
 
 import numpy as np
 import networkx as nx
