358efe44dfe68c2e4d6a73f61c4f2b01b7d5af50
YoganandaKishore <diging_yogi@sols-jessima-06.mobile.asu.edu>
2015-11-06 17:07:45 -0700

    adding test cases

--- tethne/tests/test_utilities.py
+++ tethne/tests/test_utilities.py
@@ -3,6 +3,9 @@ sys.path.append('./')
 
 import unittest
 
+from tethne.utilities import subdict
+from tethne.utilities import concat_list
+from tethne.utilities import overlap
 from tethne.utilities import _strip_punctuation
 
 class TestStripPunctuation(unittest.TestCase):
@@ -11,6 +14,40 @@ class TestStripPunctuation(unittest.TestCase):
 
     def test_unicode(self):
         self.assertEqual(u'asdf', _strip_punctuation(u'a.s,d;f?'))
+
+class TestOverlap(unittest.TestCase):
+
+    def test_empty_listA(self):
+        self.assertEqual([],overlap([1,2,3,4],None))
+
+    def test_empty_listB(self):
+        self.assertEqual([],overlap(None,[1,2,3,4]))
+
+    def test_both_not_empty(self):
+        self.assertEqual([2,4],overlap([1,2,3,4],[2,4]))
+
+class TestSubDict(unittest.TestCase):
+
+    def test_NonEmpty(self):
+        superdict = { 1:10, 2:20, 3:30, 4:40}
+        keys = {2,4}
+        test_subdict = {2:20, 4:40}
+        self.assertEqual(test_subdict,subdict(superdict,keys))
+
+class TestConcatList(unittest.TestCase):
+
+
+    def test_EqualSize_Default(self):
+        listA = ['a','c','e']
+        listB = ['b','f','h']
+        self.assertEqual(['a b','c f','e h'],concat_list(listA,listB))
+
+    def test_EqualSize(self):
+        listA = ['a','c','e']
+        listB = ['b','f','h']
+        delim = ','
+        self.assertEqual(['a,b','c,f','e,h'],concat_list(listA,listB,delim))
+
 
 
 if __name__ == '__main__':
