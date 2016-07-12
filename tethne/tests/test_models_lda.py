ce95f429f033d860d4c96984c0128a932cdf2c83
Erick Peirson <erick.peirson@asu.edu>
2016-07-12 15:27:46 -0400

    TETHNE-133 can load existing MALLET output into LDAModel

--- tethne/tests/test_models_lda.py
+++ tethne/tests/test_models_lda.py
@@ -21,121 +21,143 @@ logger = logging.getLogger('mallet')
 logger.setLevel('DEBUG')
 
 
-class TestLDAModel(unittest.TestCase):
+class TestLDAModelExistingOutput(unittest.TestCase):
     def setUp(self):
         from tethne.model.corpus.mallet import LDAModel
-        corpus = read(datapath, index_by='wosid')
-        corpus.index_feature('abstract', tokenize, structured=True)
-        self.model = LDAModel(corpus, featureset_name='abstract')
-        self.model.fit(Z=20, max_iter=500)
+        self.corpus = read(datapath, index_by='wosid')
+        self.corpus.index_feature('abstract', tokenize, structured=True)
+        self.old_model = LDAModel(self.corpus, featureset_name='abstract', nodelete=True)
+        self.old_model.fit(Z=20, max_iter=50)
 
-    def test_ldamodel(self):
-        dates, rep = self.model.topic_over_time(1)
-        self.assertGreater(sum(rep), 0)
-        self.assertEqual(len(dates), len(rep))
-
-        self.assertIsInstance(self.model.phi, FeatureSet)
-        self.assertIsInstance(self.model.theta, FeatureSet)
-
-        self.assertIsInstance(self.model.list_topics(), list)
-        self.assertGreater(len(self.model.list_topics()), 0)
-        self.assertIsInstance(self.model.list_topic(0), list)
-        self.assertGreater(len(self.model.list_topic(0)), 0)
-
-    def test_networks(self):
-        termGraph = topics.terms(self.model)
-        self.assertGreater(termGraph.size(), 100)
-        self.assertGreater(termGraph.order(), 10)
-
-        topicGraph = topics.cotopics(self.model)
-        self.assertGreater(topicGraph.size(), 5)
-        self.assertGreater(topicGraph.order(), 0)
-
-        paperGraph = topics.topic_coupling(self.model)
-        self.assertGreater(paperGraph.size(), 100)
-        self.assertGreater(paperGraph.order(), 20)
-
-
-class TestLDAModelUnstructured(unittest.TestCase):
-    def setUp(self):
+    def test_load_existing_data(self):
         from tethne.model.corpus.mallet import LDAModel
-        corpus = read(datapath, index_by='wosid')
-        corpus.index_feature('abstract', tokenize)
-        self.model = LDAModel(corpus, featureset_name='abstract')
-        self.model.fit(Z=20, max_iter=500)
-
-    def test_ldamodel(self):
-        dates, rep = self.model.topic_over_time(1)
-        self.assertGreater(sum(rep), 0)
-        self.assertEqual(len(dates), len(rep))
-
-        self.assertIsInstance(self.model.phi, FeatureSet)
-        self.assertIsInstance(self.model.theta, FeatureSet)
-
-        self.assertIsInstance(self.model.list_topics(), list)
-        self.assertGreater(len(self.model.list_topics()), 0)
-        self.assertIsInstance(self.model.list_topic(0), list)
-        self.assertGreater(len(self.model.list_topic(0)), 0)
-
-    def test_networks(self):
-        termGraph = topics.terms(self.model)
-        self.assertGreater(termGraph.size(), 100)
-        self.assertGreater(termGraph.order(), 10)
-
-        topicGraph = topics.cotopics(self.model)
-        self.assertGreater(topicGraph.size(), 5)
-        self.assertGreater(topicGraph.order(), 0)
-
-        paperGraph = topics.topic_coupling(self.model)
-        self.assertGreater(paperGraph.size(), 100)
-        self.assertGreater(paperGraph.order(), 20)
+        new_model = LDAModel(self.corpus, featureset_name='abstract',
+                             nodelete=True,
+                             prep=False,
+                             wt=self.old_model.wt,
+                             dt=self.old_model.dt,
+                             om=self.old_model.om)
+        new_model.load()
+        
+        self.assertEqual(self.old_model.topics_in(u'WOS:000295037200001'),
+                         new_model.topics_in(u'WOS:000295037200001'))
 
 
-class TestLDAModelWithTransformation(unittest.TestCase):
-    def setUp(self):
-        from tethne.model.corpus.mallet import LDAModel
-        corpus = read(datapath, index_by='wosid')
-        corpus.index_feature('abstract', tokenize)
-
-        xf = lambda f, c, C, DC: c*3
-        corpus.features['xf'] = corpus.features['abstract'].transform(xf)
-        self.model = LDAModel(corpus, featureset_name='xf')
-        self.model.fit(Z=20, max_iter=500)
-
-    def test_ldamodel(self):
-        dates, rep = self.model.topic_over_time(1)
-        self.assertGreater(sum(rep), 0)
-        self.assertEqual(len(dates), len(rep))
-
-        self.assertIsInstance(self.model.phi, FeatureSet)
-        self.assertIsInstance(self.model.theta, FeatureSet)
-
-        self.assertIsInstance(self.model.list_topics(), list)
-        self.assertGreater(len(self.model.list_topics()), 0)
-        self.assertIsInstance(self.model.list_topic(0), list)
-        self.assertGreater(len(self.model.list_topic(0)), 0)
-
-    def test_networks(self):
-        termGraph = topics.terms(self.model)
-        self.assertGreater(termGraph.size(), 100)
-        self.assertGreater(termGraph.order(), 10)
-
-        topicGraph = topics.cotopics(self.model)
-        self.assertGreater(topicGraph.size(), 5)
-        self.assertGreater(topicGraph.order(), 0)
-
-        paperGraph = topics.topic_coupling(self.model)
-        self.assertGreater(paperGraph.size(), 100)
-        self.assertGreater(paperGraph.order(), 20)
-
-
-class TestLDAModelMALLETPath(unittest.TestCase):
-    def test_direct_import(self):
-        from tethne import LDAModel
-        corpus = read(datapath, index_by='wosid')
-        corpus.index_feature('abstract', tokenize, structured=True)
-        self.model = LDAModel(corpus, featureset_name='abstract')
-        self.model.fit(Z=20, max_iter=500)
+# class TestLDAModel(unittest.TestCase):
+#     def setUp(self):
+#         from tethne.model.corpus.mallet import LDAModel
+#         corpus = read(datapath, index_by='wosid')
+#         corpus.index_feature('abstract', tokenize, structured=True)
+#         self.model = LDAModel(corpus, featureset_name='abstract')
+#         self.model.fit(Z=20, max_iter=500)
+#
+#     def test_ldamodel(self):
+#         dates, rep = self.model.topic_over_time(1)
+#         self.assertGreater(sum(rep), 0)
+#         self.assertEqual(len(dates), len(rep))
+#
+#         self.assertIsInstance(self.model.phi, FeatureSet)
+#         self.assertIsInstance(self.model.theta, FeatureSet)
+#
+#         self.assertIsInstance(self.model.list_topics(), list)
+#         self.assertGreater(len(self.model.list_topics()), 0)
+#         self.assertIsInstance(self.model.list_topic(0), list)
+#         self.assertGreater(len(self.model.list_topic(0)), 0)
+#
+#     def test_networks(self):
+#         termGraph = topics.terms(self.model)
+#         self.assertGreater(termGraph.size(), 100)
+#         self.assertGreater(termGraph.order(), 10)
+#
+#         topicGraph = topics.cotopics(self.model)
+#         self.assertGreater(topicGraph.size(), 5)
+#         self.assertGreater(topicGraph.order(), 0)
+#
+#         paperGraph = topics.topic_coupling(self.model)
+#         self.assertGreater(paperGraph.size(), 100)
+#         self.assertGreater(paperGraph.order(), 20)
+#
+#
+# class TestLDAModelUnstructured(unittest.TestCase):
+#     def setUp(self):
+#         from tethne.model.corpus.mallet import LDAModel
+#         corpus = read(datapath, index_by='wosid')
+#         corpus.index_feature('abstract', tokenize)
+#         self.model = LDAModel(corpus, featureset_name='abstract')
+#         self.model.fit(Z=20, max_iter=500)
+#
+#     def test_ldamodel(self):
+#         dates, rep = self.model.topic_over_time(1)
+#         self.assertGreater(sum(rep), 0)
+#         self.assertEqual(len(dates), len(rep))
+#
+#         self.assertIsInstance(self.model.phi, FeatureSet)
+#         self.assertIsInstance(self.model.theta, FeatureSet)
+#
+#         self.assertIsInstance(self.model.list_topics(), list)
+#         self.assertGreater(len(self.model.list_topics()), 0)
+#         self.assertIsInstance(self.model.list_topic(0), list)
+#         self.assertGreater(len(self.model.list_topic(0)), 0)
+#
+#     def test_networks(self):
+#         termGraph = topics.terms(self.model)
+#         self.assertGreater(termGraph.size(), 100)
+#         self.assertGreater(termGraph.order(), 10)
+#
+#         topicGraph = topics.cotopics(self.model)
+#         self.assertGreater(topicGraph.size(), 5)
+#         self.assertGreater(topicGraph.order(), 0)
+#
+#         paperGraph = topics.topic_coupling(self.model)
+#         self.assertGreater(paperGraph.size(), 100)
+#         self.assertGreater(paperGraph.order(), 20)
+#
+#
+# class TestLDAModelWithTransformation(unittest.TestCase):
+#     def setUp(self):
+#         from tethne.model.corpus.mallet import LDAModel
+#         corpus = read(datapath, index_by='wosid')
+#         corpus.index_feature('abstract', tokenize)
+#
+#         xf = lambda f, c, C, DC: c*3
+#         corpus.features['xf'] = corpus.features['abstract'].transform(xf)
+#         self.model = LDAModel(corpus, featureset_name='xf')
+#         self.model.fit(Z=20, max_iter=500)
+#
+#     def test_ldamodel(self):
+#         dates, rep = self.model.topic_over_time(1)
+#         self.assertGreater(sum(rep), 0)
+#         self.assertEqual(len(dates), len(rep))
+#
+#         self.assertIsInstance(self.model.phi, FeatureSet)
+#         self.assertIsInstance(self.model.theta, FeatureSet)
+#
+#         self.assertIsInstance(self.model.list_topics(), list)
+#         self.assertGreater(len(self.model.list_topics()), 0)
+#         self.assertIsInstance(self.model.list_topic(0), list)
+#         self.assertGreater(len(self.model.list_topic(0)), 0)
+#
+#     def test_networks(self):
+#         termGraph = topics.terms(self.model)
+#         self.assertGreater(termGraph.size(), 100)
+#         self.assertGreater(termGraph.order(), 10)
+#
+#         topicGraph = topics.cotopics(self.model)
+#         self.assertGreater(topicGraph.size(), 5)
+#         self.assertGreater(topicGraph.order(), 0)
+#
+#         paperGraph = topics.topic_coupling(self.model)
+#         self.assertGreater(paperGraph.size(), 100)
+#         self.assertGreater(paperGraph.order(), 20)
+#
+#
+# class TestLDAModelMALLETPath(unittest.TestCase):
+#     def test_direct_import(self):
+#         from tethne import LDAModel
+#         corpus = read(datapath, index_by='wosid')
+#         corpus.index_feature('abstract', tokenize, structured=True)
+#         self.model = LDAModel(corpus, featureset_name='abstract')
+#         self.model.fit(Z=20, max_iter=500)
 
 
 
